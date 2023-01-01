c3fa22f4cd7bf625d4059591721b794c75696698
Olivier Giniaux <oginiaux@smartadserver.com>
2023-01-02 00:13:26 +0100

    Improve circuit breaker thread safety

--- Reed/ReedExtensions.cs
+++ Reed/ReedExtensions.cs
@@ -2,23 +2,53 @@ namespace Reed;
 
 public static class ReedExtensions
 {
+    /// <summary>
+    /// A lock-free solution for atomic (composite) clamped increment
+    /// </summary>
+    /// <param name="value"></param>
+    /// <param name="max"></param>
+    /// <returns></returns>
     public static int IncrementClamped(ref int value, int max)
     {
-        int initial = Thread.VolatileRead(ref value);
-        if (initial >= max)
+        while (true)
         {
-            return max;
+            int initial = Thread.VolatileRead(ref value);
+            if (initial >= max)
+            {
+                return max;
+            }
+            int computed = initial + 1;
+            // If initial value is still what it was before, it means no other thread has changed it, thus we can consider the operation successful and return
+            if (Interlocked.CompareExchange(ref value, computed, initial) == initial)
+            {
+                return value;
+            }
+            // Retry
         }
-        return Interlocked.Increment(ref value);
     }
     
+    /// <summary>
+    /// A lock-free solution for atomic (composite) clamped decrement
+    /// </summary>
+    /// <param name="value"></param>
+    /// <param name="min"></param>
+    /// <returns></returns>
     public static int DecrementClamped(ref int value, int min)
     {
-        int initial = Thread.VolatileRead(ref value);
-        if (initial <= min)
+        while (true)
         {
-            return min;
+            int initial = Thread.VolatileRead(ref value);
+            if (initial <= min)
+            {
+                return min;
+            }
+            int computed = initial - 1;
+            // If initial value is still what it was before, it means no other thread has changed it, thus we can consider the operation successful and return
+            if (Interlocked.CompareExchange(ref value, computed, initial) == initial)
+            {
+                return value;
+            }
+            // Retry
         }
-        return Interlocked.Decrement(ref value);
     }
 }
